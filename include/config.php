547e6391d70c896864a8dba606438cceda3b0567
ruhinacse_14 <ak_limacse10@yahoo.com>
2016-08-23 12:49:51 +0600

    commite

--- include/config.php
+++ include/config.php
@@ -1,6 +1,6 @@
 <?php 
 $con = mysqli_connect("https://www.db4free.net/","ruhina05","0a4bfb","ttticketing") or die("Error " . mysqli_error($con));
 $config=array();
-$config['base_url']='https://bustrainmovie.herokuapp.com/';
+$config['base_url']='https://bustrainmovie.herokuapp.com';
 $config=(object)$config;
  ?>
