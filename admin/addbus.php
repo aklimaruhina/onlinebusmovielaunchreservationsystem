205c99bd62329b50dd1b530dbc7012ce8d0f80cd
ruhinacse_14 <ak_limacse10@yahoo.com>
2016-08-23 15:05:12 +0600

    ccc

--- admin/addbus.php
+++ admin/addbus.php
@@ -16,23 +16,12 @@ if($_POST):
 	$new_date1 = $date1[2].'-'.$date1[1].'-'.$date1[0];
 	$date2 = explode('/', $_POST['arr_date']);
 	$new_date2 = $date2[2].'-'.$date2[1].'-'.$date2[0];
-	// $date1 = $_POST['dept_date'];
-	// $date1=date('Y-m-d',strtotime($date1));
-	// $date2 = $_POST['arr_date'];
-	// $date2=date('Y-m-d',strtotime($date2));
-	// $date1 = mysqli_real_escape_string($_POST['dept_date']);
-	// $date1 = date('Y-m-d', strtotime(str_replace('-', '/', $date1)));
 
-	// $date1 = date('Y-m-d', strtotime(str_replace('-', '/', $date1)));
-	// $dept_date = $_POST['dept_date'];
-	// $date2= mysqli_real_escape_string($_POST['arr_date']);
-	// $date2 = date('Y-m-d', strtotime(str_replace('-', '/', $date2)));
-
-	// $arr_date = $_POST['arr_date'];
 	$query = "INSERT INTO `bus_reserve` (`id`, `bus_name`, `bus_info`, `city_from`, `city_to`, `seat`, `fare`, `dtime`, `arrtime`, `dept_date`, `arr_date`,`reserve_seat_id`) VALUES (NULL, '$bus_name', '$bus_info', '$city_from', '$city_to', '$seat', '$fare', '$dt', '$at', '$new_date1', '$new_date2','$reserve_id')";
 	$result = mysqli_query($con, $query);
 	
 	if(!$result){
+		var_dump($query);
 		echo "You have error in your database........";
 		echo "<br>";
 	}
