d96095ed011fab76ac1bece756d8b90e5249aabb
ruhinacse_14 <ak_limacse10@yahoo.com>
2016-08-23 12:46:37 +0600

    updated data

--- include/config.php
+++ include/config.php
@@ -1,6 +1,6 @@
 <?php 
-$con = mysqli_connect("localhost","root","","book_myshow") or die("Error " . mysqli_error($con));
+$con = mysqli_connect("https://www.db4free.net/","ruhina05","0a4bfb","ttticketing") or die("Error " . mysqli_error($con));
 $config=array();
-$config['base_url']='http://localhost/project-1';
+$config['base_url']='https://bustrainmovie.herokuapp.com/';
 $config=(object)$config;
  ?>
