94aed27fd817821693182aa364151cc31de94d3b
ruhinacse_14 <ak_limacse10@yahoo.com>
2016-08-23 13:15:08 +0600

    change on config file

--- include/config.php
+++ include/config.php
@@ -1,5 +1,5 @@
 <?php 
-$con = mysqli_connect("https://www.db4free.net","ruhina05","0a4bfb","ttticketing") or die("Error " . mysqli_error($con));
+$con = mysqli_connect("www.db4free.net","ruhina05","0a4bfb","ttticketing") or die("Error " . mysqli_error($con));
 $config=array();
 $config['base_url']='https://bustrainmovie.herokuapp.com';
 $config=(object)$config;
