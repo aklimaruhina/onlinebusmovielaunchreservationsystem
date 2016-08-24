3721c886611c11c7a632ce646f77382e96b1220f
ruhinacse_14 <ak_limacse10@yahoo.com>
2016-08-25 00:36:12 +0600

    cmooi

--- include/config.php
+++ include/config.php
@@ -1,6 +1,17 @@
 <?php 
-$con = mysqli_connect("www.db4free.net","ruhina05","0a4bfb","ttticketing") or die("Error " . mysqli_error($con));
+$con = mysqli_connect("www.db4free.net","ruhina05","0a4bfb","ttticketing");
+if (mysqli_connect_errno()) {
+    printf("Connect failed: %s\n", mysqli_connect_error());
+    exit();
+}
+
+if (!mysqli_query($con, "SET a=1")) {
+    printf("Errormessage: %s\n", mysqli_error($con));
+}
+/* close connection */
 $config=array();
 $config['base_url']='https://bustrainmovie.herokuapp.com';
 $config=(object)$config;
+mysqli_close($con);
+
  ?>
